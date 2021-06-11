a5978a50961244aece50cd60bc7756313cbc51ea
k-gn <rlarbska97@gmail.com>
2021-06-11 19:58:00 +0900

    무한참조 방지

--- workspace/photogram/src/main/java/com/cos/photogramstart/domain/likes/Likes.java
+++ workspace/photogram/src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -14,6 +14,7 @@ import javax.persistence.UniqueConstraint;
 
 import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -45,6 +46,7 @@ public class Likes {
 	@ManyToOne
 	private Image image;
 	
+	@JsonIgnoreProperties({"images"})
 	@JoinColumn(name = "userId")
 	@ManyToOne
 	private User user;
